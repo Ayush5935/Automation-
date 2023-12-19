777340d48825b5d3c5f29c1252ed6603ced6d97b
Ayush Kumar Sharma <64814485+Ayush5935@users.noreply.github.com>
2023-12-19 11:54:40 +0530

    Update Krishna.py
    Added 2code check which one is working

--- Krishna.py
+++ Krishna.py
@@ -53,3 +53,71 @@ if __name__ == '__main__':
 
     copy_ami(args.source, args.source_region, args.ami, args.target, args.target_region)
 ￼Enter
+
+
+
+
+---------
+
+import argparse
+import boto3
+from botocore.exceptions import WaiterError
+
+def copy_ami(source_account_id, source_region, source_ami_id, destination_account_id, destination_region):
+    kms_key_id = 'arn:aws:kms:us-west-2:346687249423:key/3cd4107b-98d1-486a-972c-b4734c735a69'
+
+    # Create sessions for source and destination accounts
+    source_session = boto3.Session(region_name=source_region)
+    destination_session = boto3.Session(region_name=destination_region)
+
+    # Create clients for EC2 in both source and destination accounts
+    ec2_source = source_session.client('ec2', region_name=source_region)
+    ec2_destination = destination_session.client('ec2', region_name=destination_region)
+
+    # Copy the AMI to the destination account
+    response = ec2_source.copy_image(
+        SourceImageId=source_ami_id,
+        SourceRegion=source_region,
+        Name='CopiedAMI',
+        Encrypted=True,
+        KmsKeyId=kms_key_id
+    )
+
+    copied_ami_id = response['ImageId']
+    print(f"Copying AMI {source_ami_id} to {destination_account_id}... Please wait...")
+
+    try:
+        # Wait for the AMI to be available in the source account
+        ec2_source.get_waiter('image_available').wait(ImageIds=[source_ami_id])
+    except WaiterError as e:
+        print(f"Error waiting for AMI in source account: {e}")
+
+    try:
+        # Wait for the AMI to be available in the destination account
+        ec2_destination.get_waiter('image_available').wait(ImageIds=[copied_ami_id])
+    except WaiterError as e:
+        print(f"Error waiting for copied AMI in destination account: {e}")
+
+    # Describe the copied AMI in the destination account
+    copied_ami = ec2_destination.describe_images(ImageIds=[copied_ami_id])['Images'][0]
+
+    print(f"Copied AMI ID: {copied_ami['ImageId']}")
+    print(f"AMI Name: {copied_ami['Name']}")
+    print(f"Snapshot ID: {copied_ami['BlockDeviceMappings'][0]['Ebs']['SnapshotId']}")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Copy AMI from source account to destination account.')
+    parser.add_argument('--source_account', type=str, help='Source AWS account ID')
+    parser.add_argument('--source_region', type=str, help='Source AWS region')
+    parser.add_argument('--source_ami', type=str, help='Source AMI ID to copy')
+    parser.add_argument('--destination_account', type=str, help='Destination AWS account ID')
+    parser.add_argument('--destination_region', type=str, help='Destination AWS region')
+    args = parser.parse_args()
+
+    copy_ami(
+        source_account_id=args.source_account,
+        source_region=args.source_region,
+        source_ami_id=args.source_ami,
+        destination_account_id=args.destination_account,
+        destination_region=args.destination_region
+    )
