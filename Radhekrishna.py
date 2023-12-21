f1afb171ee1f7d8dc663eaf76ee357da0be6c8fa
Ayush Kumar Sharma <64814485+Ayush5935@users.noreply.github.com>
2023-12-21 19:47:18 +0530

    Update Radhekrishna.py

--- Radhekrishna.py
+++ Radhekrishna.py
@@ -315,3 +315,120 @@ def delete_instance_in_target(target, region, instance_id):
     sts_client = boto3
 
 
+
+
+
+
+import argparse
+import boto3
+from botocore.exceptions import WaiterError
+
+def copy_ami(source_account_id, source_region, source_ami_id, destination_account_id, destination_region):
+    kms_key_id = 'arn:aws:kms:us-west-2:346687249423:key/3cd4107b-98d1-486a-972c-b4734c735a69'
+    source_session = boto3.Session(region_name=source_region)
+    destination_session = boto3.Session(region_name=destination_region)
+    ec2_source = source_session.client('ec2', region_name=source_region)
+
+    # Copy AMI in source account
+    response = ec2_source.copy_image(
+        SourceImageId=source_ami_id,
+        SourceRegion=source_region,
+        Name='CopiedAMI',
+        Encrypted=True,
+        KmsKeyId=kms_key_id
+    )
+    copied_ami_id = response['ImageId']
+    print(f"Copying AMI {source_ami_id} to source account... Please wait...")
+
+    try:
+        ec2_source.get_waiter('image_available').wait(ImageIds=[source_ami_id])
+    except WaiterError as e:
+        print(f"Error waiting for copied AMI in source account: {e}")
+
+    ec2_destination = destination_session.client('ec2', region_name=destination_region)
+
+    # Share AMI with destination account
+    response_share_ami = ec2_source.modify_image_attribute(
+        ImageId=source_ami_id,
+        LaunchPermission={
+            'Add': [{'UserId': destination_account_id}]
+        }
+    )
+
+    print(f"Sharing AMI {source_ami_id} with destination account... Please wait...")
+
+    try:
+        ec2_destination.get_waiter('image_available').wait(ImageIds=[copied_ami_id])
+    except WaiterError as e:
+        print(f"Error waiting for copied AMI in destination account: {e}")
+
+    # Describe the copied AMI in the destination account
+    try:
+        copied_ami_destination = ec2_destination.describe_images(ImageIds=[copied_ami_id])['Images'][0]
+        print(f"Copied AMI ID in destination account: {copied_ami_destination['ImageId']}")
+        print(f"AMI Name in destination account: {copied_ami_destination['Name']}")
+        print(f"Snapshot ID in destination account: {copied_ami_destination['BlockDeviceMappings'][0]['Ebs']['SnapshotId']}")
+    except Exception as e:
+        print(f"Error describing copied AMI in destination account: {e}")
+
+    return copied_ami_destination
+
+def delete_ami(ec2_client, ami_id):
+    try:
+        ec2_client.deregister_image(ImageId=ami_id)
+        print(f"Deleted AMI: {ami_id}")
+    except Exception as e:
+        print(f"Error deleting AMI {ami_id}: {e}")
+
+def delete_snapshot(ec2_client, snapshot_id):
+    try:
+        ec2_client.delete_snapshot(SnapshotId=snapshot_id)
+        print(f"Deleted Snapshot: {snapshot_id}")
+    except Exception as e:
+        print(f"Error deleting Snapshot {snapshot_id}: {e}")
+
+def delete_instance(ec2_client, instance_id):
+    try:
+        ec2_client.terminate_instances(InstanceIds=[instance_id])
+        print(f"Terminated instance: {instance_id}")
+    except Exception as e:
+        print(f"Error terminating instance {instance_id}: {e}")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Copy AMI from source account to destination account.')
+    parser.add_argument('--source', type=str, help='Source AWS account ID')
+    parser.add_argument('--source_region', type=str, help='Source AWS region')
+    parser.add_argument('--ami', type=str, help='Source AMI ID to copy')
+    parser.add_argument('--target', type=str, help='Destination AWS account ID')
+    parser.add_argument('--target_region', type=str, help='Destination AWS region')
+    args = parser.parse_args()
+
+    copied_ami_destination = copy_ami(args.source, args.source_region, args.ami, args.target, args.target_region)
+
+    # User confirmation for deletion
+    delete_copied_ami = input("Do you want to delete the copied AMI in source account? (Y/N): ").lower()
+
+    if delete_copied_ami == 'y':
+        ec2_source = boto3.Session(region_name=args.source_region).client('ec2')
+        delete_ami(ec2_source, copied_ami_destination['ImageId'])
+        delete_snapshot(ec2_source, copied_ami_destination['BlockDeviceMappings'][0]['Ebs']['SnapshotId'])
+
+    delete_instance_in_target = input("Do you want to delete the instance created in the target account? (Y/N): ").lower()
+
+    if delete_instance_in_target == 'y':
+        # Assuming you have the instance_id stored from the earlier part of the script
+        instance_id_to_delete = "your_instance_id"
+        ec2_target = boto3.Session(region_name=args.target_region).client('ec2')
+        delete_instance(ec2_target, instance_id_to_delete)
+
+    delete_new_ami_in_target = input("Do you want to delete the new AMI created in the target account? (Y/N): ").lower()
+
+    if delete_new_ami_in_target == 'y':
+        # Assuming you have the new_ami_id stored from the earlier part of the script
+        new_ami_id_to_delete = "your_new_ami_id"
+        delete_ami(ec2_target, new_ami_id_to_delete)
+
+    print("Script execution completed.")
+
+
+
