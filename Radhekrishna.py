63d71e3163a2108d287dce89517cd9019788eb5d
Ayush Kumar Sharma <64814485+Ayush5935@users.noreply.github.com>
2023-12-28 19:43:02 +0530

    Update Radhekrishna.py

--- Radhekrishna.py
+++ Radhekrishna.py
@@ -208,3 +208,103 @@ if __name__ == '__main__':
 
     # Use the obtained SSO credentials to copy the AMI between accounts
     copy_ami(args.source, args.source_region, args.ami, args.target, args.target_region, sso_access_token, sso_session_obj)
+
+import boto3
+import argparse
+from botocore.exceptions import WaiterError
+from aws_requests_auth.boto_utils import BotoAWSRequestsAuth
+from time import sleep
+import requests
+
+region = 'us-west-2'
+
+def sso_session():
+    session = boto3.Session()
+    start_url = 'https://d-92670ca28f.awsapps.com/start#/'
+    sso_oidc = session.client('sso-oidc', region_name="us-west-2")
+    client_creds = sso_oidc.register_client(
+        clientName='myapp',
+        clientType='public',
+    )
+    device_authorization = sso_oidc.start_device_authorization(
+        clientId=client_creds['clientId'],
+        clientSecret=client_creds['clientSecret'],
+        startUrl=start_url,
+    )
+    url = device_authorization['verificationUriComplete']
+    device_code = device_authorization['deviceCode']
+    expires_in = device_authorization['expiresIn']
+    interval = device_authorization['interval']
+
+    print(f"Open the following URL in your browser to authenticate: {url}")
+    print(f"Waiting for authentication...")
+
+    for n in range(1, expires_in // interval + 1):
+        sleep(interval)
+        try:
+            token_response = requests.post(
+                "https://d-92670ca28f.awsapps.com/token",
+                data={
+                    'grant_type': 'urn:ietf:params:oauth:grant-type:device_code',
+                    'device_code': device_code,
+                    'client_id': client_creds['clientId'],
+                    'client_secret': client_creds['clientSecret'],
+                }
+            )
+            token_response.raise_for_status()
+            token = token_response.json()
+            access_token = token['access_token']
+            break
+        except requests.exceptions.HTTPError as e:
+            if e.response.status_code == 400 and 'authorization_pending' in e.response.text:
+                pass
+            else:
+                raise
+
+    print("Authentication successful!")
+    return access_token, session
+
+def assume_role_with_sso(target, region, sso_access_token, sso_session):
+    sts_client = sso_session.client('sts')
+    role_arn = f"arn:aws:iam::{target}:role/ami_copy_role"
+    
+    try:
+        assumed_role = sts_client.assume_role_with_web_identity(
+            RoleArn=role_arn,
+            RoleSessionName="AssumedRoleSession",
+            WebIdentityToken=sso_access_token
+        )
+        
+        return boto3.client(
+            'ec2',
+            region_name=region,
+            aws_access_key_id=assumed_role['Credentials']['AccessKeyId'],
+            aws_secret_access_key=assumed_role['Credentials']['SecretAccessKey'],
+            aws_session_token=assumed_role['Credentials']['SessionToken']
+        )
+    except sts_client.exceptions.InvalidIdentityTokenException as e:
+        print(f"Error: InvalidIdentityTokenException - {e}")
+        raise
+    except Exception as e:
+        print(f"Error: An unexpected error occurred - {e}")
+        raise
+
+# Rest of your code remains unchanged
+# ...
+
+# Entry point of the script
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Copy AMI from source account to destination account.')
+    parser.add_argument('--source', type=str, help='Source AWS account ID')
+    parser.add_argument('--source_region', type=str, help='Source AWS region')
+    parser.add_argument('--ami', type=str, help='Source AMI ID to copy')
+    parser.add_argument('--target', type=str, help='Destination AWS account ID')
+    parser.add_argument('--target_region', type=str, help='Destination AWS region')
+    args = parser.parse_args()
+
+    # Call the function to get and use AWS SSO credentials
+    sso_access_token, sso_session_obj = sso_session()
+
+    # Use the obtained SSO credentials to copy the AMI between accounts
+    copy_ami(args.source, args.source_region, args.ami, args.target, args.target_region, sso_access_token, sso_session_obj)
+
