cb2690f390d512ea782ef0156131e9754fe292a4
Ayush Kumar Sharma <64814485+Ayush5935@users.noreply.github.com>
2024-01-02 17:22:06 +0530

    Update Hosted.py

--- Hosted.py
+++ Hosted.py
@@ -8,13 +8,13 @@ def csv_to_json(csv_file_path):
         csv_reader = csv.DictReader(csvfile)
         
         for row in csv_reader:
-            hosted_zone_name = row['Hosted Zone Name']
-            record_type = row['Type']
+            hosted_zone_name = row['Hosted Zone Name'].lower()  # Convert to lowercase
+            record_type = row['Type'].lower()  # Convert to lowercase
             created_by = row['Created By']
             record_count = row['Record Count']
             hosted_zone_id = row['Hosted Zone ID']
-            environment = row['Environment']
-            region = row['Region']
+            environment = row['Environment'].lower()  # Convert to lowercase
+            region = row['Region'].lower()  # Convert to lowercase
 
             if hosted_zone_name not in json_data:
                 json_data[hosted_zone_name] = {}
@@ -22,9 +22,14 @@ def csv_to_json(csv_file_path):
             if environment not in json_data[hosted_zone_name]:
                 json_data[hosted_zone_name][environment] = {}
 
-            json_data[hosted_zone_name][environment][region] = {
-                "value": f"/hostedzone/{hosted_zone_id}"
-            }
+            if region:
+                json_data[hosted_zone_name][environment][region] = {
+                    "value": f"/hostedzone/{hosted_zone_id}"
+                }
+            else:
+                json_data[hosted_zone_name][environment] = {
+                    "value": f"/hostedzone/{hosted_zone_id}"
+                }
 
     return json_data
 
@@ -36,4 +41,3 @@ def save_json(json_data, json_file_path):
 csv_file_path = 'your_csv_file.csv'
 json_data = csv_to_json(csv_file_path)
 save_json(json_data, 'output.json')
-￼Enter
