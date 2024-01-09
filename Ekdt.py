fdfe2042cff0e1388f5760737e78b239cfe9bf33
Ayush Kumar Sharma <64814485+Ayush5935@users.noreply.github.com>
2024-01-09 15:57:29 +0530

    Create Ekdt.py

---
+++ Ekdt.py
@@ -0,0 +1,91 @@
+import os
+import boto3
+import json
+import uuid
+from kubernetes import client, config
+
+def get_eks_cluster_endpoint(cluster_name):
+    eks_client = boto3.client('eks')
+    response = eks_client.describe_cluster(name=cluster_name)
+    return response['cluster']['endpoint']
+
+def get_running_pod_details():
+    config.load_kube_config()
+    k8s_api = client.CoreV1Api()
+    pods = k8s_api.list_pod_for_all_namespaces()
+    running_pod_details = []
+
+    for pod in pods.items:
+        if pod.status.phase == 'Running' and pod.metadata.namespace == 'twistlock':
+            running_pod_details.append({
+                'Namespace': pod.metadata.namespace,
+                'Name': pod.metadata.name,
+                'Ready': pod.status.container_statuses[0].ready if pod.status.container_statuses else None,
+                'Status': pod.status.phase,
+            })
+
+    return running_pod_details
+
+def get_nodes_count():
+    config.load_kube_config()
+    k8s_api = client.CoreV1Api()
+    nodes = k8s_api.list_node()
+    return len(nodes.items)
+
+def assume_role_and_update_dynamodb(cluster_name, running_pod_details):
+    assume_role_arn = 'arn:aws:iam::155880749572:role/5g-defender-installation-automation'
+    sts_client = boto3.client('sts')
+    assumed_role = sts_client.assume_role(
+        RoleArn=assume_role_arn,
+        RoleSessionName='AssumedRoleSession'
+    )
+
+    dynamodb = boto3.resource('dynamodb', aws_access_key_id=assumed_role['Credentials']['AccessKeyId'],
+                              aws_secret_access_key=assumed_role['Credentials']['SecretAccessKey'],
+                              aws_session_token=assumed_role['Credentials']['SessionToken'])
+
+    table_name = 'twistlock-defender-cluster-version'
+    table = dynamodb.Table(table_name)
+
+    unique_id = '670cd21b-4a42-11ee-9d3c-5feaf422a957'  # Use your logic to generate a unique ID
+    print(f'Updating DynamoDb item with id {unique_id} and eksclustername {cluster_name}')
+
+    response = table.update_item(
+        Key={'id': unique_id, 'eksClusterName': cluster_name},
+        UpdateExpression='SET IsEqual = :val, RunningPodDetails = :pod_details',
+        ExpressionAttributeValues={':val': True, ':pod_details': running_pod_details},
+        ReturnValues='ALL_NEW'
+    )
+
+    updated_item = response.get('Attributes', {})
+    print(f'Updated DynamoDB Item: {updated_item}')
+
+def lambda_handler(event, context):
+    try:
+        cluster_name = 'cc-ndc-eks-cluster-dev-cluster'
+        cluster_endpoint = get_eks_cluster_endpoint(cluster_name)
+        running_pod_details = get_running_pod_details()
+        nodes_count = get_nodes_count()
+        is_equal = len(running_pod_details) == nodes_count
+
+        assume_role_and_update_dynamodb(cluster_name, running_pod_details)
+
+        print(f'Number of Nodes in the Cluster: {nodes_count}')
+        print('Running Pod Details:')
+        for pod in running_pod_details:
+            print(f' Namespace: {pod["Namespace"]}')
+            print(f' Name: {pod["Name"]}')
+            print(f' Ready: {pod["Ready"]}')
+            print(f' Status: {pod["Status"]}')
+            print("----------")
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps({'ClusterEndpoint': cluster_endpoint, 'RunningPodDetails': running_pod_details, 'NodesCount': nodes_count})
+        }
+    except Exception as e:
+        print(f"Error in lambda_handler: {e}")
+        raise
+
+# Uncomment the line below to test the lambda locally
+# lambda_handler(None, None)
