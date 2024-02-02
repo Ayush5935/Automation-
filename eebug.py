ab30b846caf38a622440ed3042ef4fb8ab7bef87
Ayush Kumar Sharma <64814485+Ayush5935@users.noreply.github.com>
2024-02-02 23:21:24 +0530

    Update eebug.py

--- eebug.py
+++ eebug.py
@@ -5,7 +5,7 @@ from dash import html, dcc
 from dash.dependencies import Input, Output
 from dash_cytoscape import Cytoscape
 import dash_bootstrap_components as dbc
-from dash_iconify import DashIconify
+from dash_iconify import Icon
 
 def fetch_aws_data(account, region, ipv4, eni, subnet, route_table, destination_ipv4, tgw):
     # (Your existing code for fetching AWS data)
@@ -19,19 +19,17 @@ def aws_network_graph(eni_details, subnet_details, route_table_details, tgw_deta
     tgw_attachment_id = tgw_attachments[0]["TransitGatewayAttachmentId"]
     tgw_route_table_id = tgw_attachments[0]['Association'].get('TransitGatewayRouteTableId')
 
-    elements.append({'data': {'id': 'eni', 'label': f'ENI {eni_id}', 'type': 'ENI', 'icon': 'https://img.icons8.com/ios-filled/50/000000/aws.png'}})
-    elements.append({'data': {'id': 'subnet', 'label': f'Subnet {subnet_id}'}})
-    elements.append({'data': {'id': 'route_table', 'label': f'Route Table {route_table_id}'}})
-    elements.append({'data': {'id': 'tgw', 'label': f'Transit Gateway {tgw_id}'}})
-    elements.append({'data': {'id': 'tgw_rtb', 'label': f'Tgw Route Table ID {tgw_route_table_id}'}})
-    elements.append({'data': {'id': 'tgw_attachment', 'label': f'Transit Gateway Attachment {tgw_attachment_id}'}})
+    elements.append({'data': {'id': 'eni', 'label': f'ENI {eni_id}', 'type': 'ENI', 'icon': 'aws:network-adapter'}})
+    elements.append({'data': {'id': 'subnet', 'label': f'Subnet {subnet_id}', 'icon': 'aws:network' }})
+    elements.append({'data': {'id': 'route_table', 'label': f'Route Table {route_table_id}', 'icon': 'aws:route-table' }})
+    elements.append({'data': {'id': 'tgw', 'label': f'Transit Gateway {tgw_id}', 'icon': 'aws:transit-gateway' }})
+    elements.append({'data': {'id': 'tgw_rtb', 'label': f'Tgw Route Table ID {tgw_route_table_id}', 'icon': 'aws:route-table' }})
+    elements.append({'data': {'id': 'tgw_attachment', 'label': f'Transit Gateway Attachment {tgw_attachment_id}', 'icon': 'aws:attachment' }})
     elements.append({'data': {'source': 'eni', 'target': 'subnet'}})
     elements.append({'data': {'source': 'subnet', 'target': 'route_table'}})
     elements.append({'data': {'source': 'route_table', 'target': 'tgw'}})
     elements.append({'data': {'source': 'tgw', 'target': 'tgw_attachment'}})
     elements.append({'data': {'source': 'tgw_attachment', 'target': 'tgw_rtb'}})
-
-    elements.append({'data': {'id': 'github_icon', 'label': 'GitHub', 'type': 'GitHub', 'icon': 'ion:logo-github'}})
 
     app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
@@ -43,18 +41,16 @@ def aws_network_graph(eni_details, subnet_details, route_table_details, tgw_deta
             dbc.Col(
                 Cytoscape(
                     id='graph',
-                    layout={'name': 'circle'},
+                    layout={'name': 'grid'},
                     style={'width': '100%', 'height': '600px'},
                     elements=elements,
                     stylesheet=[
                         {'selector': 'node', 'style': {'content': 'data(label)', 'font-size': '12px', 'width': '90px', 'height': '90px', 'shape': 'ellipse', 'text-halign': 'center', 'text-valign': 'bottom'}},
-                        {'selector': '#eni', 'style': {'background-image': 'url(https://img.icons8.com/ios-filled/50/000000/aws.png)', 'background-fit': 'cover', 'background-width': '70%', 'background-height': '70%', 'border-color': '#3573A5'}},
-                        {'selector': '#subnet', 'style': {'background-color': '#98FB98', 'border-color': '#4CAF50'}},
-                        {'selector': '#route_table', 'style': {'background-color': '#FFD700', 'border-color': '#FFC107'}},
-                        {'selector': '#tgw', 'style': {'background-color': '#FF6347', 'border-color': '#E57373'}},
-                        {'selector': '#tgw_attachment', 'style': {'background-color': '#8A2BE2', 'border-color': '#7B1FA2'}},
-                        {'selector': '#tgw_rtb', 'style': {'background-color': '#30c8d9', 'border-color': '#30c8d9'}},
-                        {'selector': '#github_icon', 'style': {'background-image': 'url(https://ionicons.com/ionicons/ion:logo-github.svg)', 'background-fit': 'cover', 'background-width': '70%', 'background-height': '70%', 'border-color': '#3573A5'}},
+                        {'selector': '[type = "ENI"]', 'style': {'background-color': '#6FB1FC', 'border-color': '#3573A5'}},
+                        {'selector': '[type = "Subnet"]', 'style': {'background-color': '#98FB98', 'border-color': '#4CAF50'}},
+                        {'selector': '[type = "Route Table"]', 'style': {'background-color': '#FFD700', 'border-color': '#FFC107'}},
+                        {'selector': '[type = "Transit Gateway"]', 'style': {'background-color': '#FF6347', 'border-color': '#E57373'}},
+                        {'selector': '[type = "Transit Gateway Attachment"]', 'style': {'background-color': '#8A2BE2', 'border-color': '#7B1FA2'}},
                         {'selector': 'edge', 'style': {'width': 3, 'line-color': '#9DB5B2', 'curve-style': 'bezier', 'target-arrow-shape': 'triangle'}}
                     ]
                 ),
@@ -63,13 +59,7 @@ def aws_network_graph(eni_details, subnet_details, route_table_details, tgw_deta
         ]),
         dbc.Row([
             dbc.Col(
-                DashIconify(
-                    icon="ion:logo-github",
-                    width=30,
-                    rotate=1,
-                    flip="horizontal",
-                ),
-                className="mt-4"
+                html.Div(id='node-info', className="mt-4")
             )
         ])
     ])
