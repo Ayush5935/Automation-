c4df115c56bc732285ac3829b9df8d95ace2f3a6
Ayush Kumar Sharma <64814485+Ayush5935@users.noreply.github.com>
2024-02-06 12:25:11 +0530

    Update H.py

--- H.py
+++ H.py
@@ -1,45 +1,40 @@
-from aws_cdk import (
-    aws_codebuild as codebuild,
-    aws_events as events,
-    aws_events_targets as targets,
-    aws_s3 as s3,
-    core,
-)
+import boto3
 
-class MyHelmChartVersioningStack(core.Stack):
-
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
-        super().__init__(scope, id, **kwargs)
-
-        # S3 bucket for Helm charts
-        bucket = s3.Bucket(self, "HelmChartBucket")
-
-        # CodeBuild project
-        codebuild.Project(
-            self, "HelmChartBuild",
-            source=codebuild.Source.s3(bucket=bucket, path="path/to/source.zip"),
-            build_spec=codebuild.BuildSpec.from_source_filename("buildspec.yml"),
-            environment=codebuild.BuildEnvironment(
-                build_image=codebuild.LinuxBuildImage.STANDARD_4_0
-            )
-        )
-
-        # S3 event rule to trigger CodeBuild directly
-        rule = events.Rule(
-            self, "HelmChartRule",
-            event_pattern={
-                "source": ["aws.s3"],
-                "detail": {
-                    "eventName": ["PutObject"]
-                },
-                "resources": [bucket.bucket_arn],
+def lambda_handler(event, context):
+    # Extract bucket and key from S3 event
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    key = event['Records'][0]['s3']['object']['key']
+    
+    # Start CodeBuild project
+    codebuild_client = boto3.client('codebuild')
+    response = codebuild_client.start_build(
+        projectName='your-codebuild-project-name',
+        environmentVariablesOverride=[
+            {
+                'name': 'S3_BUCKET',
+                'value': bucket
+            },
+            {
+                'name': 'ZIP_FILE_KEY',
+                'value': key
             }
-        )
-        rule.add_target(targets.CodeBuildProject(
-            project=codebuild.Project.from_project_name(self, "CodeBuildProject", "HelmChartBuild"))
-        )
+        ]
+    )
+    print(response)
 
-app = core.App()
-MyHelmChartVersioningStack(app, "MyHelmChartVersioningStack")
-app.synth()
-￼Enter
+
+
+
+version: 0.2
+
+phases:
+  install:
+    runtime-versions:
+      python: 3.8
+  build:
+    commands:
+      - aws s3 cp s3://$S3_BUCKET/$ZIP_FILE_KEY .
+      - unzip $ZIP_FILE_KEY
+      - # Extract Helm chart or perform other necessary steps
+      - # Push Helm chart to ECR
+
