f85fda25954ac6f732b228db6028cec33b93f497
Ayush Kumar Sharma <64814485+Ayush5935@users.noreply.github.com>
2024-02-08 15:14:04 +0530

    Update Traffic.py

--- Traffic.py
+++ Traffic.py
@@ -4,10 +4,16 @@ import csv
 # Initialize Boto3 client
 client = boto3.client('networkmanager')
 
-# Function to retrieve mirror sessions details
-def get_mirror_sessions():
+# Function to retrieve global network ID
+def get_global_network_id():
     response = client.describe_global_networks()
-    global_network_id = response['GlobalNetworks'][0]['GlobalNetworkId']
+    if 'GlobalNetworks' in response and response['GlobalNetworks']:
+        return response['GlobalNetworks'][0]['GlobalNetworkId']
+    else:
+        raise Exception("No global network found in the AWS account.")
+
+# Function to retrieve mirror sessions details
+def get_mirror_sessions(global_network_id):
     response = client.get_transit_gateway_registrations(GlobalNetworkId=global_network_id)
     transit_gateway_arn = response['TransitGatewayRegistrations'][0]['TransitGatewayArn']
     response = client.describe_transit_gateway_attachments(Filters=[{'Name': 'transit-gateway-arn', 'Values': [transit_gateway_arn]}])
@@ -17,9 +23,7 @@ def get_mirror_sessions():
     return mirror_sessions
 
 # Function to retrieve mirror targets details
-def get_mirror_targets():
-    response = client.describe_global_networks()
-    global_network_id = response['GlobalNetworks'][0]['GlobalNetworkId']
+def get_mirror_targets(global_network_id):
     response = client.get_transit_gateway_registrations(GlobalNetworkId=global_network_id)
     transit_gateway_arn = response['TransitGatewayRegistrations'][0]['TransitGatewayArn']
     response = client.describe_transit_gateway_attachments(Filters=[{'Name': 'transit-gateway-arn', 'Values': [transit_gateway_arn]}])
@@ -29,9 +33,7 @@ def get_mirror_targets():
     return mirror_targets
 
 # Function to retrieve mirror filters details
-def get_mirror_filters():
-    response = client.describe_global_networks()
-    global_network_id = response['GlobalNetworks'][0]['GlobalNetworkId']
+def get_mirror_filters(global_network_id):
     response = client.get_transit_gateway_registrations(GlobalNetworkId=global_network_id)
     transit_gateway_arn = response['TransitGatewayRegistrations'][0]['TransitGatewayArn']
     response = client.describe_transit_gateway_attachments(Filters=[{'Name': 'transit-gateway-arn', 'Values': [transit_gateway_arn]}])
@@ -48,17 +50,21 @@ def export_to_csv(data, filename):
         for item in data:
             writer.writerow(item.values())
 
-# Get mirror sessions details
-mirror_sessions = get_mirror_sessions()
-export_to_csv(mirror_sessions, 'mirror_sessions.csv')
+try:
+    global_network_id = get_global_network_id()
 
-# Get mirror targets details
-mirror_targets = get_mirror_targets()
-export_to_csv(mirror_targets, 'mirror_targets.csv')
+    # Get mirror sessions details
+    mirror_sessions = get_mirror_sessions(global_network_id)
+    export_to_csv(mirror_sessions, 'mirror_sessions.csv')
 
-# Get mirror filters details
-mirror_filters = get_mirror_filters()
-export_to_csv(mirror_filters, 'mirror_filters.csv')
+    # Get mirror targets details
+    mirror_targets = get_mirror_targets(global_network_id)
+    export_to_csv(mirror_targets, 'mirror_targets.csv')
 
-print("CSV files exported successfully.")
-￼Enter
+    # Get mirror filters details
+    mirror_filters = get_mirror_filters(global_network_id)
+    export_to_csv(mirror_filters, 'mirror_filters.csv')
+
+    print("CSV files exported successfully.")
+except Exception as e:
+    print("Error:", e)
